ba5f7b45ad1ef3cbfa4e619d2a819f7d6cc12cd2
Helmut Hummel <typo3@helhum.io>
2024-10-31 09:49:28 +0000

    Update extension based on commit cb5eb4b06bef10a1cd457198a21dde6b9bc62384

--- Resources/Private/ExtensionArtifacts/Libraries/composer/installed.php
+++ Resources/Private/ExtensionArtifacts/Libraries/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'typo3-console/libs',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a91eb93190fa1cb23f94c244fa97477f81a2eaa7',
+        'reference' => '368ed55b2876fd8ffa9f81ece7f8ddf1d8d29262',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,7 +31,7 @@
         'typo3-console/libs' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a91eb93190fa1cb23f94c244fa97477f81a2eaa7',
+            'reference' => '368ed55b2876fd8ffa9f81ece7f8ddf1d8d29262',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
